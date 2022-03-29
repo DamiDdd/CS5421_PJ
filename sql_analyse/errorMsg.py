074963be27d22cf5c3e81fda111ee44a195d4c37
Qjj980819 <834767813@qq.com>
2022-03-29 19:50:10 +0800

    sql_analyse v1.0

---
+++ sql_analyse/errorMsg.py
@@ -0,0 +1,2 @@
+syntaxMsg = "Syntax error in "
+IOMsg = "IO error: couldn't find "
---
+++ sql_analyse/linkDataset.py
@@ -0,0 +1,74 @@
+import psycopg2
+import errorMsg as msg
+import re
+
+
+def exe_sql_with_res(conn, sql):
+    pointer = conn.cursor()
+    try:
+        pointer.execute(sql)
+    except psycopg2.Error:
+        print(msg.syntaxMsg + sql)
+    else:
+        res = pointer.fetchall()
+        return res
+
+
+def analyse_sql(conn, sql, exe_time=20):
+    sql_test = "SELECT test('" + sql + "', " + str(exe_time) + ")"
+    time_list = exe_sql_with_res(conn, sql_test)
+    res = 0
+    if time_list is not None:
+        for time_tuple in time_list:
+            time_strs = re.findall(r"\d+\.?\d*", str(time_tuple))
+            res += float(time_strs[1])
+    return res
+
+
+def exe_sql(conn, sql):
+    pointer = conn.cursor()
+    try:
+        pointer.execute(sql)
+    except psycopg2.Error:
+        print(msg.syntaxMsg)
+
+
+def exe_sql_file(conn, filepath):
+    try:
+        f = open(filepath, 'r')
+    except IOError:
+        print(msg.IOMsg + filepath)
+    else:
+        str = f.readlines()
+        str = "".join(str)
+        exe_sql(conn, str)
+
+
+def setup(database, password, user="postgres", host="localhost", port="5432"):
+    conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    # init test function
+    exe_sql_file(conn, 'testFunction.sql')
+    return conn
+
+
+def uninstall(conn):
+    conn.commit()
+    conn.close()
+
+
+# connect, input your own config here
+conn = setup("PJ1", "123456")
+
+
+sql_test1 = "SELECT per.empid, per.lname " \
+            "FROM employee per FULL OUTER JOIN payroll pay  " \
+            "ON per.empid = pay.empid AND pay.salary = 189170 " \
+            "WHERE per.empid = pay.empid " \
+            "ORDER BY per.empid, per.lname;"
+sql_res1 = exe_sql_with_res(conn, sql_test1)
+print(sql_res1)
+time = analyse_sql(conn, sql_test1, 100)
+print(time)
+
+# close the connection
+uninstall(conn)
