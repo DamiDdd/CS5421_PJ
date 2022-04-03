bae9453751c66c0e3f349e721d4495a69c74637a
Limin shi <445667701@qq.com>
2022-04-03 10:54:29 +0800

    finished_3_funcs

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any secret key'
 conn = sqlite3.connect(DATABASE)
 sql_create_Competition_table = """ CREATE TABLE IF NOT EXISTS Competition (
-                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                     name CHAR(25) NOT NULL,
                                     description VARCHAR(255),
                                     start_time INT,
@@ -26,7 +26,7 @@ sql_create_Competition_table = """ CREATE TABLE IF NOT EXISTS Competition (
                                 ); """
 #what is submission_ts?
 sql_create_Submission_table = """ CREATE TABLE IF NOT EXISTS Submission (
-                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                     participant_id INT NOT NULL,
                                     submission_ts INT,
                                     query VARCHAR(1000),
@@ -36,21 +36,32 @@ sql_create_Submission_table = """ CREATE TABLE IF NOT EXISTS Submission (
                                 ); """
 
 sql_create_Participant_table = """ CREATE TABLE IF NOT EXISTS Participant (
-                                    id INT PRIMARY KEY AUTO_INCREMENT,
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                                     submission_count INT
                                 ); """
+def insert_competition():
+    sql = ''' INSERT INTO competition(name,description,start_time,end_time,answer_json,type)
+              VALUES(123,123,132,123,123,123) '''
+    c = conn.cursor()
+    c.execute(sql)
+    conn.commit()
+    c.close()
+#use it when u need examples
 
 def create_tables():
     c = conn.cursor()
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    print(c .fetchall())
-    if not c .fetchall():#no table create
+    #print(c.fetchall())
+    if not c.fetchall():#no table create
+        print('inside')
         c.execute(sql_create_Competition_table)
         c.execute(sql_create_Submission_table)
         c.execute(sql_create_Participant_table)
+    c.close()
     conn.commit()
-    conn.close()
+    #conn.close()
 create_tables()
+#insert_competition()
 
 
 @app.route("/")
@@ -59,16 +70,53 @@ def home():
 
 @app.route('/get_competition', methods=['POST'])
 def get_competition_by_id(competition_id):
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM Competition WHERE id={str(competition_id)};")
+    res = c.fetchall()
+    c.close()
+    conn.commit()
+    if not res:
+        print('Not exists in database')
+        return None
+    id,name,des,start,end,_,_ = res[0]
+    return id,name,des,start,end
+get_competition_by_id(3) #test case
 
-    return None
 
 @app.route('/list_competitions', methods=['POST'])
-def list_competitions():
-    return None
+def list_competitions(froma,to):
+    c = conn.cursor()
+    all_comp = []
+    for i in range(froma,to+1):
+        c.execute(f"SELECT * FROM Competition WHERE id={str(i)};")
+        res = c.fetchall()
+        if res:
+            id,name,des,start,end,_,_ = res[0]
+            all_comp.append((id,name,des,start,end))
+    c.close()
+    conn.commit()
+    print(all_comp)
+    return all_comp
+
+list_competitions(0,10) #test case
 
 @app.route('/add_competition', methods=['POST'])
-def add_competition():
-    return None
+def add_competition(name,description,start_time,end_time,answer_json,type):
+    c = conn.cursor()
+    sql = f''' INSERT INTO competition(name,description,start_time,end_time,answer_json,type)
+              VALUES({name},{description},{start_time},{end_time},{answer_json},{type}) '''
+
+    try:
+        c.execute(sql)
+        c.close()
+        conn.commit()
+        return True
+    except:
+        return False
+
+
+#add_competition(00,00,00,00,00,0)
+
 
 @app.route('/add_submission', methods=['POST'])
 def add_submission():
