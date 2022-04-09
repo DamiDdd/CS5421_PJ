98df4745fe92d0174b2a930b1399a7ddd0fb0e6e
Bryan Lim <bryan.lyy123@gmail.com>
2022-04-09 22:26:38 +0800

    f

--- main.py
+++ main.py
@@ -288,13 +288,16 @@ def list_submissions_by_competition():
     conn.commit()
 
     minOrMax = "MIN"
+    order = "ASC"
     if comp_type == 2:
         minOrMax = "MAX"
+        order = "DESC"
     c = conn.cursor()
-    sql = f"""SELECT participant_id, submission_status,{minOrMax}(time_spent) as FROM submission
+    sql = f"""SELECT participant_id, submission_status,{minOrMax}(time_spent) as result FROM submission
               WHERE competition_id={str(competition_id)}
               AND submission_status = 2
               GROUP BY participant_id
+              ORDER BY result {order}
                """
     print(sql)
     c.execute(sql)
